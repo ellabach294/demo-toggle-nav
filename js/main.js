b8fbe31fd16ef3861a55d3221e3c1911c8965711
Ella Bach <chipbach294@gmail.com>
2023-03-09 21:01:29 -0700

    demo complete for toggle navigation

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Step 1: select the element that will be clicked on to show or hide the navigator. In this case it's the button element. 
+const btn = document.querySelector('.toggle-btn');
+
+// Step 2: add a click event to that button icon
+btn.addEventListener('click', function(){
+    //when the button is clicked, show the nav element by  adding the class name .show-nav to it
+    document.querySelector('nav').classList.toggle('show-nav');
+});
+
+// Step 3: link to the js file in the HTML, note: the js must be loaded last
